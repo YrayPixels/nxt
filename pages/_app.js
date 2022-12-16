d1d791cf8bb70e8b0bc9ba3959399afe4d5f65c5
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2022-12-16 16:12:26 +0100

    fixed routing

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/style.css'
 import '../styles/sidenav.css'
 import '../styles/dashmain.css'
 import Script from 'next/script'
+import '../styles/otp.css'
+import Head from 'next/head'
+
 
 
 
@@ -10,7 +13,14 @@ import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
-    return <Component {...pageProps} />
+    return <Component {...pageProps}>
+
+    </Component>
+
+
+
+
+
 
 
 }
@@ -18,17 +28,7 @@ function MyApp({ Component, pageProps }) {
 export default MyApp
 
 
-// var urlencoded = new URLSearchParams();
-// urlencoded.append("email", "ibbu@gmail.com");
-// urlencoded.append("password", "12345678");
 
-// var requestOptions = {
-//     method: 'POST',
-//     body: urlencoded,
-//     redirect: 'follow'
-// };
 
-// fetch("https://stockmgt.gapaautoparts.com/api/center/login", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
+
+
